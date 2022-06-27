ecaf0443cb18640311232c68b98bbc9ace34cb78
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-06-27 12:58:40 +0700

    Sử dụng Hệ thống Mô-đun Node

--- app.js
+++ app.js
@@ -1,46 +1,9 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    //...
-    const url = req.url;
-    const method = req.method;
+console.log(routes.someText);
 
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST">');
-        res.write('<input type="text" name="message"><button type="submit">Send</button>');
-        res.write('</form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Nodejs server</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
 
