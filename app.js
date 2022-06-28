0622821ec746e1da38a1d97e239ff0565b202c87
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-06-28 09:48:07 +0700

    Phân tích cú pháp Requests đến

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("Always run");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In add product middleware");
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+    '<form method="POST" action="/product">' +
+      '<input type="text" name="title">' +
+      '<button type="submit">Add Product</button>' +
+      "</form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In / middleware");
   res.send("<h1>Hello from Express</h1>");
 });
 
