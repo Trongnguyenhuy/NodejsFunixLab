ffe8007ea71edde64bcb9caad65ae320d8861ecd
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-07-03 10:49:55 +0700

    Tìm nạp các sản phẩm liên quan

--- controllers/admin.js
+++ controllers/admin.js
@@ -37,8 +37,12 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product) => {
+  req.user.getProducts({ where: { id: prodId}})
+  // Product.findByPk(prodId)
+    .then((products) => {
+      
+      const product = products[0];
+
       if (!product) {
         return res.redirect("/");
       }
