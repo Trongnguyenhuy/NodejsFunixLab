218583e4a661ba7e6c53431f64f0a944307ee0eb
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-07-03 14:28:46 +0700

    Xóa các mặt hàng liên quan & Xóa sản phẩm giỏ hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -88,7 +88,7 @@ exports.postCart = (req, res, next) => {
         through: { qty: newQuantity },
       });
     })
-    .then(()=>{
+    .then(() => {
       res.redirect("/cart");
     })
     .catch((err) => {
@@ -112,9 +112,19 @@ exports.getOrders = (req, res, next) => {
 
 exports.postCartDeleteItem = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(req.body);
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: prodId }  });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then( result => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
