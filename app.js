333983e91c1ccf11b05b033eed4dcf00e6913a93
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-07-03 10:27:36 +0700

    Tạo và quản lý người dùng tạm thời

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const adminRouteres = require("./routes/admin");
 const shopRouteres = require("./routes/shop");
 const errorControllers = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -16,18 +18,40 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
 app.use("/admin", adminRouteres);
 app.use(shopRouteres);
 
 app.use(errorControllers.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({ name: "John", email: "john@example.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
   });
-
