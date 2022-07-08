1da8f723be90b8f0f521b75e37edba6701b05927
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-07-08 11:56:05 +0700

    Hiển thị các mặt hàng trong giỏ hàng

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get("/products", shopControllers.getProducts);
 
 router.get("/products/:productId", shopControllers.getProduct);
 
-// router.get("/cart", shopControllers.getCart);
+router.get("/cart", shopControllers.getCart);
 
 router.post("/cart", shopControllers.postCart);
 
