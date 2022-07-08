8a3a013ea5360e734ff8285371fe85a033985570
Trong Nguyen <trongnguyenhuy7@gmail.com>
2022-07-08 09:59:51 +0700

    Làm việc trên các mặt hàng & đơn đặt hàng trong giỏ hàng

--- models/user.js
+++ models/user.js
@@ -2,14 +2,28 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-  constructor(username, email, id) {
+  constructor(username, email, id, cart) {
     this.username = username;
     this.email = email;
+    this._id = id;
+    this.cart = cart;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
